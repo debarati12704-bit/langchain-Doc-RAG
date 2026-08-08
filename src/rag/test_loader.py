60b1bbf1c130c17e3c48d769a3a1d4b6b228aa0f
debarati12704-bit <debarati12704@gmail.com>
2026-08-08 21:17:45 +0530

    Add chunking (completed)

--- src/rag/test_loader.py
+++ src/rag/test_loader.py
@@ -1,5 +1,5 @@
 from rag.loaders.web_loader import load_web_pages
-
+from rag.processing.chunking import split_documents
 
 urls = [
     "https://docs.langchain.com/oss/python/langchain/overview",
@@ -10,7 +10,11 @@ documents = load_web_pages(urls)
 
 print("Number of documents:", len(documents))
 
-for document in documents:
-    print("\nSource:", document.metadata["source"])
-    print("Characters:", len(document.page_content))
-    print("content:", document.page_content[:100],"...")
+chunks=split_documents(documents)
+
+print(f"no. of chunks:{len(chunks)}")
+for i,chunk in enumerate(chunks[:2]):
+    print(f"\n\n-----Chunk{i+1}-----")
+    print(f"Chunk content:{chunk.page_content}")
+    print(f"Chunk metadata: {chunk.metadata["source"]}")
+    print(f"Characters in chunk :{len(chunk.page_content)}")
